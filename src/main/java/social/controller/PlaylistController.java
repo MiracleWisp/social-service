18dc2a47c18262e39106ba9f385c5662ed940229
Semyon Uchvatov <s-uchvatov@yandex.ru>
2019-03-26 22:06:31 +0300

    add CRUD endpoints for chat playlist

---
+++ src/main/java/social/controller/PlaylistController.java
@@ -0,0 +1,97 @@
+package social.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import social.dto.Response;
+import social.dto.TrackDTO;
+import social.entity.Chat;
+import social.entity.User;
+import social.service.ChatService;
+import social.service.UserService;
+
+@RestController
+@RequestMapping("/chats/{chatId}/playlist")
+@EnableAutoConfiguration
+public class PlaylistController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ChatService chatService;
+
+    @GetMapping
+    public ResponseEntity<?> getChatPlaylist(@PathVariable("chatId") Integer chatId,
+                                             @RequestHeader(value = "username") String username) {
+
+        Chat chat = chatService.findChatByChatId(chatId);
+        if (chat == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new Response(false, "Chat does not exist"));
+        }
+        User currentUser = userService.findUserByUsername(username);
+        if (!chat.getParticipants().contains(currentUser)) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(new Response(false, "You are not in this chat"));
+        }
+        return ResponseEntity.ok(new Response(true, chat.getPlaylist()));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addTrackToChatPlaylist(@RequestBody TrackDTO track,
+                                                    @PathVariable("chatId") Integer chatId,
+                                                    @RequestHeader(value = "username") String username) {
+        Chat chat = chatService.findChatByChatId(chatId);
+        if (chat == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new Response(false, "Chat does not exist"));
+        }
+        User currentUser = userService.findUserByUsername(username);
+        if (!chat.getParticipants().contains(currentUser)) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(new Response(false, "You are not in this chat"));
+        }
+        if (chat.getPlaylist().contains(track.getTrackId())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new Response(false, "Track already exist in playlist"));
+        }
+
+        chat.getPlaylist().add(track.getTrackId());
+        chat = chatService.saveChat(chat);
+        return ResponseEntity.ok(new Response(true, chat.getPlaylist()));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> removeTrackFromChatPlaylist(@RequestBody TrackDTO track,
+                                                    @PathVariable("chatId") Integer chatId,
+                                                    @RequestHeader(value = "username") String username) {
+        Chat chat = chatService.findChatByChatId(chatId);
+        if (chat == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new Response(false, "Chat does not exist"));
+        }
+        User currentUser = userService.findUserByUsername(username);
+        if (!chat.getOwner().equals(currentUser)) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(new Response(false, "You are not chat owner"));
+        }
+        if (!chat.getPlaylist().contains(track.getTrackId())) {
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new Response(false, "This track is not in the playlist."));
+        }
+        chat.getPlaylist().remove(track.getTrackId());
+        chat = chatService.saveChat(chat);
+        return ResponseEntity.ok(new Response(true, chat.getPlaylist()));
+    }
+}
---
+++ src/main/java/social/dto/TrackDTO.java
@@ -0,0 +1,8 @@
+package social.dto;
+
+import lombok.Data;
+
+@Data
+public class TrackDTO {
+    Integer trackId;
+}
